46b36cb6dd94ae3c7ebeaa086dde0b1fff367422
Motishjain <motishj@ymail.com>
2016-04-09 13:44:22 +0530

    post method fix

--- routes/saveRewards.js
+++ routes/saveRewards.js
@@ -3,38 +3,27 @@ var mongoose = require('mongoose');
 var OutletToRewards = require('../models/outletToRewards');
 
 var jsonResponse = function(success, data, msg) {
-	return '{success:'+success+',data:'+data+',msg:'+msg+',}';
+	return '{success:' + success + ',data:' + data + ',msg:' + msg + ',}';
 };
 
 module.exports = function(app, appEnv) {
 
-	app.post("/saveRewards",function(req, res) {
+	app.post("/saveRewards", function(req, res) {
 		var jsonRequest = req.body;
 		if (jsonRequest.outletCode) {
-			OutletToRewards.find({
-				'outletCode' : req.outletCode
+			OutletToRewards.findOneAndUpdate({
+				'outletCode' : jsonRequest.outletCode
+			}, {
+				'categoryRewards' : jsonRequest.rewardsMap
+			}, {
+				upsert : true
 			}, function(err, outletToRewards) {
 				if (err) {
-					res.json(jsonResponse(false,null,err));
+					res.json(jsonResponse(false, null, err));
 				}
-				outletToRewards.categoryRewards = jsonRequest.rewardsMap;
-				outletToReward.save(function(err) {
-					if (err) {
-						res.json(jsonResponse(false,null,err));
-					}
-				});
-				res.json(jsonResponse(true,outletToReward.outletCode,"Rewards saved Successfully"));
+				res.json(jsonResponse(true, jsonRequest.outletCode,
+						"Rewards saved Successfully"));
 			});
-		} else {
-			var outletToReward = new OutletToRewards();
-			outletToReward.outletCode = jsonRequest.outletCode;
-			outletToReward.categoryRewards = jsonRequest.rewardsMap;
-			outletToReward.save(function(err) {
-				if (err) {
-					res.json(jsonResponse(false,null,err));
-				}
-			});
-			res.json(jsonResponse(true,outletToReward.outletCode,"Rewards saved Successfully"));
 		}
 
 	});
