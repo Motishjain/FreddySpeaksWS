353af965e936b14fa7918ce92babe7726463f291
Motishjain <motishj@ymail.com>
2016-04-19 13:43:08 +0530

    ws fixes

--- routes/fetchFeedback.js
+++ routes/fetchFeedback.js
@@ -5,36 +5,47 @@ var OutletFeedback = models.OutletFeedback;
 var Feedback = models.Feedback;
 var outletFeedbackList;
 
-
 module.exports = function(app, appEnv) {
-	
-	app.get("/fetchFeedback/:outletCode/:fromDate/:toDate",function(req, res) {
-		
+
+	app.get("/fetchFeedback/:outletCode/:fromDate/:toDate", function(req, res) {
+
 		var outletCode = req.params.outletCode;
 		var fromDate = new Date(req.params.fromDate);
 		var toDate = new Date(req.params.toDate);
-		
+
 		if (outletCode) {
-			OutletFeedback.findOne({
-				'outletCode' : outletCode,
-				'feedbackList.createdDate' : {"$gte": fromDate, "$lt": toDate}
-			}, function(err, outletFeedback) {
-				if (err) {
-					res.send(err);
+			OutletFeedback.aggregate([ {
+				$match : {
+					'outletCode' : outletCode
 				}
-				outletFeedbackList = outletFeedback.feedbackList;
-				res.json(outletFeedbackList);
-			});	
+			}, {
+				$project : {
+					"feedbackList" : 1
+				}
+			}, {
+				$unwind : '$feedbackList'
+			}, {
+				$match : {
+					'feedbackList.createdDate' : {
+						'$gte' : fromDate,
+						'$lte' : toDate
+					}
+				}
+			} ], function(err, result) {
+				if (err) {
+					console.log(err);
+					return;
+				}
+				res.json(result);
+			});
 		}
-			
-		
+
 	});
 };
 
-
 /*
-Object.keys(o).forEach(function(key) {
-var val = o[key];
-logic();
-});
-*/
+ Object.keys(o).forEach(function(key) {
+ var val = o[key];
+ logic();
+ });
+ */
