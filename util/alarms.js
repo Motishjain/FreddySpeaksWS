98684c19c6d04232e1df7e4d69602ec40d88d391
Motishjain <motishj@ymail.com>
2016-05-15 17:29:42 +0530

    alarm setup

--- util/alarms.js
+++ util/alarms.js
@@ -3,31 +3,34 @@ var OutletSubscription = require('../models/outletSubscription');
 var Constants = require('../util/constants');
 var currentDate = new Date();
 var gcm = require('node-gcm');
-var currentOutlet;
 
 function updateSubscription(err, outletSubscription) {
 	if (err) {
 		console.log(err);
 		return;
 	}
-	var daysOverDue;
-	
+	var daysOverDue, daysLeft=0, timeDiff;
+
 	var expirationDate = outletSubscription.expiryDate;
 	var newStatus;
-    if(outletSubscription.activationStatus!==Constants.SUBSCRIPTION_EXPIRED) {
-        if(currentDate>expirationDate) {
-            var diff = currentDate.getTime() - expirationDate.getTime();
-            daysOverDue = diff / (1000 * 60 * 60 * 24);
-            if(daysOverDue<7) {
-                newStatus = Constants.SUBSCRIPTION_PENDING;
-            }
-            else {
-                newStatus = Constants.SUBSCRIPTION_EXPIRED;
-            }
-        }
-    }
-    if(newStatus!==null) {
-    	OutletSubscription.findOneAndUpdate({
+	if(outletSubscription.activationStatus!==Constants.SUBSCRIPTION_EXPIRED) {
+		if(currentDate>expirationDate) {
+			timeDiff = currentDate.getTime() - expirationDate.getTime();
+			daysOverDue = timeDiff / (1000 * 60 * 60 * 24);
+			if(daysOverDue<7) {
+				newStatus = Constants.SUBSCRIPTION_PENDING;
+			}
+			else {
+				newStatus = Constants.SUBSCRIPTION_EXPIRED;
+			}
+		}
+		else {
+			timeDiff = expirationDate.getTime() - currentDate.getTime();
+			daysLeft = timeDiff / (1000 * 60 * 60 * 24);
+		}
+	}
+	if(newStatus) {
+		OutletSubscription.findOneAndUpdate({
 			'outletCode' : outletSubscription.outletCode			    				
 		}, {
 			'activationStatus' : newStatus
@@ -37,46 +40,54 @@ function updateSubscription(err, outletSubscription) {
 				return;
 			}
 		});
-    }
-    
-    if(newStatus===Constants.SUBSCRIPTION_PENDING || newStatus===Constants.SUBSCRIPTION_EXPIRED) {
-    	var gcmToken = currentOutlet.gcmToken;
-    	var notificationTitle, notificationBody;
-    	if(newStatus===Constants.SUBSCRIPTION_PENDING) {
-    		notificationTitle = 'Renewal Pending';    	
-    		notificationBody = 'Expires in'+ (7-daysOverDue) + 'days';
-    	}
-    	else {
-    		notificationTitle = 'Subscription Expired';
-    		notificationBody = 'Please renew your subscription to continue.';
-    	}
-    	
-    	var message = new gcm.Message({    		
-    		priority: 'high',
-    		contentAvailable: true,
-    		data: {
-    			outletCode: currentOutlet.outletCode,
-    			activationStatus: newStatus
-    		},
-    		notification: {
-    			title: notificationTitle,
-    			icon: "ic_launcher",
-    			body: notificationBody
-    		}
-    	});
-    	
-    	var regTokens = [gcmToken];
-    	var sender = new gcm.Sender(Constants.GCM_SERVER_KEY);
-    	sender.send(message, { registrationTokens: regTokens }, function (err, response) {
-    	    if(err) {
-    	    	console.error(err);
-    	    }
-    	    else {
-    	    	console.log(response);
-    	    }
-    	});
-    }
-	
+		outletSubscription.activationStatus = newStatus;
+	}
+
+	if(newStatus===Constants.SUBSCRIPTION_PENDING || newStatus===Constants.SUBSCRIPTION_EXPIRED || daysLeft<3) {
+		
+		Outlet.findOne({'outletCode':outletSubscription.outletCode}, function(err, currentOutlet) {
+			var gcmToken = currentOutlet.gcmToken;
+			var notificationTitle, notificationBody;
+			if(outletSubscription.activationStatus===Constants.SUBSCRIPTION_PENDING) {
+				notificationTitle = 'Renewal Pending';    	
+				notificationBody = 'Expires in '+ (7-daysOverDue) + ' days';
+			}
+			else if(outletSubscription.activationStatus===Constants.SUBSCRIPTION_EXPIRED) {
+				notificationTitle = 'Subscription Expired';
+				notificationBody = 'Please renew your subscription to continue.';
+			}
+			else {
+				notificationTitle = 'Gentle Reminder';    	
+				notificationBody = 'Your subscription will expire in '+ daysLeft + ' days';
+			}
+
+			var message = new gcm.Message({    		
+				priority: 'high',
+				contentAvailable: true,
+				data: {
+					outletCode: outletSubscription.outletCode,
+					activationStatus: outletSubscription.activationStatus
+				},
+				notification: {
+					title: notificationTitle,
+					icon: "ic_launcher",
+					body: notificationBody
+				}
+			});
+
+			var regTokens = [gcmToken];
+			var sender = new gcm.Sender(Constants.GCM_SERVER_KEY);
+			sender.send(message, { registrationTokens: regTokens }, function (err, response) {
+				if(err) {
+					console.error(err);
+				}
+				else {
+					console.log(response);
+				}
+			});
+		});
+	}
+
 }
 
 module.exports = {
@@ -86,8 +97,8 @@ module.exports = {
 					console.log("Error finding outlets");
 				}
 				for (var i = 0, len = outlets.length; i < len; i++) {
-					currentOutlet = outlets[i];
-					OutletSubscription.findOne({'outletCode':currentOutlet.outletCode}, updateSubscription);
+					var outlet = outlets[i];
+					OutletSubscription.findOne({'outletCode':outlet.outletCode}, updateSubscription);
 				}
 
 			});
